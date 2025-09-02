a9a70f010893aa846b667da604025a854b7168fa
BrunaGavioli <Brunagavioli1326@gmail.com>
2025-09-02 19:02:30 -0300

    Calculando_quantidade.py

---
+++ Calculando_quantidade.py
@@ -0,0 +1,12 @@
+tijolos = 25.000
+areia = 25
+cimento = 150
+
+valor_tijolos = float(tijolos*2.50)
+print(f" O custo total é de R$ {valor_tijolos} mil")
+
+valor_areia = float(areia*5.00)
+print(f"O custo total é de R$ {valor_areia} mil ")
+
+valor_cimento = float(cimento*50.00)
+print(f" O custo total é de R$ {valor_cimento} mil")
