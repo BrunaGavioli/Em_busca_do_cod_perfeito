69d2e4a09c299013b2b47b8e873b22cf8da1c2db
BrunaGavioli <Brunagavioli1326@gmail.com>
2025-09-02 19:07:04 -0300

    Calculando_litragem_de_aquario.py

---
+++ Calculando_litragem_de_aquario.py
@@ -0,0 +1,7 @@
+comprimento = int(input("Digite o valor de comprimento: "))
+largura = int(input("Digite o valor de largura: "))
+altura = int(input("Digite o valor de altura: "))
+
+
+litragem = (comprimento*largura*altura)
+print(f"O tamanho do aquario é {litragem}")
